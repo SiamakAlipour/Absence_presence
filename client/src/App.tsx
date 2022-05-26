fa235e9b524a3fef8f4d038f6871705e12919c67
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-26 19:05:36 +0430

    some changes done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,143 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import api from './api';
-import axios from 'axios';
+import { BrowserRouter as BRouter } from 'react-router-dom';
 import './App.css';
-import User from './User';
+import Router from './Router';
 
-type UsersProps = {
-	_id?: string;
-	username: string;
-	status: boolean;
-};
 function App() {
-	const [users, setUsers] = useState<UsersProps[]>([
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: true,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-		{
-			username: 'test',
-			status: false,
-		},
-	]);
-	const [loading, setLoading] = useState(true);
-	const [error, setError] = useState(null);
-
-	useEffect(function fetchUsersList() {
-		(async () =>
-			await api
-				.get('/auth')
-				.then((res) => setUsers([...users, ...res.data.users]))
-				.catch((err) => setError(err))
-				.finally(() => setLoading(false)))();
-	}, []);
-
-	if (error) return <h1>{`error`}</h1>;
 	return (
-		<div className='app'>
-			{loading && <h1>loading...</h1>}
-			<div className='app__users-list'>
-				{users.map((user, index) => (
-					<User username={user.username} status={user.status} />
-				))}
+		<BRouter>
+			<div className='app'>
+				<Router />
 			</div>
-		</div>
+		</BRouter>
 	);
 }
 
