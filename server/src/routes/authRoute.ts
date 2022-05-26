ec79bc0769cc14c696ab29906aa33e68f783ab3c
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-26 10:20:18 +0430

    folder structure changed

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -2,23 +2,27 @@ import express, { Request, Response } from 'express';
 import User from '../models/User';
 const route = express.Router();
 
-route.get('/login', (req, res) => {
-	res.send('hello');
+route.get('/', async (req: Request, res: Response) => {
+	const users = await User.find();
+	res.status(200).send({ msg: 'Users list', users });
 });
+
 route.post('/login', async (req: Request, res: Response) => {
 	const { username, password } = req.body;
-	console.log(username, password);
-	// const userExist = await User.findOne({ username: username.toLowerCase() });
-	// if (userExist) {
-	// 	return;
-	// }
+	const userExist = await User.findOne({ username: username.toLowerCase() });
+	if (userExist) {
+		return res.status(400).send({
+			msg: 'User already exist',
+			username: userExist.username,
+		});
+	}
 	const newUser = new User({
 		username: username.toLowerCase(),
 		password,
 	});
 	try {
 		await newUser.save();
-		res.status(200).send({ newUser });
+		res.status(200).send({ msg: 'User Created', username: newUser.username });
 	} catch (error) {
 		res.status(400).send({ error });
 	}
