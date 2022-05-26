54797b3753b07b1de440094b0ae816bc806dd7c9
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-26 23:27:18 +0430

    some changes done

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,6 +1,24 @@
+import { API_URL } from './../constants/index';
 import axios from 'axios';
 import config from '../config.json';
 
 const api = axios.create({ baseURL: config.api });
 
-export default api;
+type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
+
+type Request = (url: string, method: Method, data: any) => Promise<unknown>;
+
+const request: Request = (url, method, data) => {
+	switch (method) {
+		case 'GET':
+			return new Promise((resolve, reject) => {
+				axios
+					.get(API_URL + url)
+					.then((res) => resolve(res.data))
+					.catch((err) => reject(err));
+			});
+		default:
+			return Promise.reject(new Error());
+	}
+};
+export default request;
