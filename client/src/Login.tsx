65dbde06993d57046000cd46f0d9a6a87d9b0006
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-26 19:16:49 +0430

    Login component update

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './Login.css';
 function Login() {
 	return (
-		<div className='login'>
-			<h1 className='badge bg-dark fs-3 w-100'>Welcome</h1>
+		<div className='login p-3 w-100'>
+			<h1 className='badge bg-dark fs-3'>Welcome</h1>
 			<form action='' className='row form-control'>
-				<input type='text' placeholder='username' className='form-input mt-3' />
+				<input
+					type='text'
+					placeholder='username'
+					className='form-control mt-3'
+				/>
 				<input
 					type='password'
 					placeholder='password'
-					className='form-input mt-3'
+					className='form-control mt-3'
 				/>
 				<button type='submit' className='btn btn-primary mt-4'>
 					Login
