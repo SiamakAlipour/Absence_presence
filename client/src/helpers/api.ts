944d820220352c21914e78a91ae68acdc24d4d88
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-27 14:40:28 +0430

    Post case added

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,19 +1,22 @@
-import { API_URL } from './../constants/index';
+import { Method } from './../constants/types/index';
+import { API_URL, POST, GET } from './../constants/index';
 import axios from 'axios';
-import config from '../config.json';
-
-const api = axios.create({ baseURL: config.api });
-
-type Method = 'GET' | 'POST' | 'PATCH' | 'DELETE';
 
 type Request = (url: string, method: Method, data: any) => Promise<unknown>;
 
 const request: Request = (url, method, data) => {
 	switch (method) {
-		case 'GET':
+		case GET:
 			return new Promise((resolve, reject) => {
 				axios
 					.get(API_URL + url)
+					.then((res) => resolve(res.data))
+					.catch((err) => reject(err));
+			});
+		case POST:
+			return new Promise((resolve, reject) => {
+				axios
+					.post(API_URL + url, data)
 					.then((res) => resolve(res.data))
 					.catch((err) => reject(err));
 			});
