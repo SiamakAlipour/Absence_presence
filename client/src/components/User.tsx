945f9c8edc4665c1886c6bc5c934e51f959d044b
siamak alipour <siamakalipour37@gmail.com>
2022-05-29 12:38:29 +0430

    some changes done

--- client/src/components/User.tsx
+++ client/src/components/User.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
-import './User.css';
+import React from "react";
+import "./User.css";
 type UserProps = {
-	username: string;
-	status: boolean;
+  username: string;
+  status: boolean;
 };
 
 function User({ username, status }: UserProps) {
-	return (
-		<div className='user'>
-			{/* username */}
-			<p>{username}</p>
-			{/* status */}
-			{status ? (
-				<span className='badge bg-success'>Present</span>
-			) : (
-				<span className='badge bg-danger'>Absent</span>
-			)}
-		</div>
-	);
+  return (
+    <div className="user">
+      {/* username */}
+      <p>{username}</p>
+      {/* status */}
+      {status ? (
+        <span className="badge bg-success p-2">Present</span>
+      ) : (
+        <span className="badge bg-danger p-2">Absent</span>
+      )}
+    </div>
+  );
 }
 
 export default User;
--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,44 +1,44 @@
-import express, { Request, Response } from 'express';
-import User from '../models/User';
+import express, { Request, Response } from "express";
+import User from "../models/User";
 const route = express.Router();
 
-route.get('/', async (req: Request, res: Response) => {
-	const users = await User.find();
-	res.status(200).send({ msg: 'Users list', users });
+route.get("/", async (req: Request, res: Response) => {
+  const users = await User.find();
+  res.status(200).send({ msg: "Users list", users });
 });
 
-route.post('/login', async (req: Request, res: Response) => {
-	const { username, password, status = false } = req.body;
-	const userExist = await User.findOne({ username: username.toLowerCase() });
-	if (userExist) {
-		await User.updateOne(
-			{ _id: userExist._id },
-			{
-				$set: {
-					status: true,
-				},
-			}
-		);
-		return res.status(200).send({
-			msg: 'User already exist, Welcome',
-			username: userExist.username,
-		});
-	}
-	const newUser = new User({
-		username: username.toLowerCase(),
-		password,
-		status: true,
-	});
-	try {
-		await newUser.save();
-		res.status(200).send({
-			msg: 'User Created, Welcome',
-			username: newUser.username,
-			status,
-		});
-	} catch (error) {
-		res.status(400).send({ error });
-	}
+route.post("/login", async (req: Request, res: Response) => {
+  const { username, password, status = false } = req.body;
+  const userExist = await User.findOne({ username: username.toLowerCase() });
+  if (userExist) {
+    await User.updateOne(
+      { _id: userExist._id },
+      {
+        $set: {
+          status: true,
+        },
+      }
+    );
+    return res.status(200).send({
+      msg: "User already exist, Welcome",
+      username: userExist.username,
+    });
+  }
+  const newUser = new User({
+    username: username.toLowerCase(),
+    password,
+    status: true,
+  });
+  try {
+    await newUser.save();
+    res.status(200).send({
+      msg: "User Created, Welcome",
+      username: newUser.username,
+      status,
+    });
+  } catch (error) {
+    res.status(400).send({ error });
+  }
 });
 
 export default route;
