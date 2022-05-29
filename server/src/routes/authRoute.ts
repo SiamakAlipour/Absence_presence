3df74699088a11a94ef00487ddb37440af595c2e
Siamak Alipour <siamakalipour37@gmail.com>
2022-05-29 20:50:28 +0430

    bug fixed

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,51 +1,56 @@
-import express, { Request, Response } from "express";
-import User from "../models/User";
+import express, { Request, Response } from 'express';
+import User from '../models/User';
 const route = express.Router();
 
-route.get("/", async (req: Request, res: Response) => {
-  const users = await User.find();
-  res.status(200).send({ msg: "Users list", users });
+route.get('/', async (req: Request, res: Response) => {
+	const users = await User.find();
+	res.status(200).send({ msg: 'Users list', users });
 });
 
-route.post("/login", async (req: Request, res: Response) => {
-  const { username, password, status = false } = req.body;
-  const userExist = await User.findOne({ username: username.toLowerCase() });
-  if (userExist) {
-    await User.updateOne(
-      { _id: userExist._id },
-      {
-        $set: {
-          status: true,
-        },
-      }
-    );
-    return res.status(200).send({
-      msg: "User already exist, Welcome",
-      username: userExist.username,
-    });
-  }
-  const newUser = new User({
-    username: username.toLowerCase(),
-    password,
-    status: true,
-  });
-  try {
-    await newUser.save();
-    res.status(200).send({
-      msg: "User Created, Welcome",
-      username: newUser.username,
-      status,
-    });
-  } catch (error) {
-    res.status(400).send({ error });
-  }
+route.post('/login', async (req: Request, res: Response) => {
+	const { username, password, status = false } = req.body;
+	const userExist = await User.findOne({ username: username.toLowerCase() });
+
+	if (userExist) {
+		if (userExist.password !== password) {
+			console.log(userExist.password);
+			return res.status(400).send({ msg: 'Invalid Password' });
+		}
+		await User.updateOne(
+			{ _id: userExist._id },
+			{
+				$set: {
+					status: true,
+				},
+			}
+		);
+		return res.status(200).send({
+			msg: 'User already exist, Welcome',
+			username: userExist.username,
+		});
+	}
+	const newUser = new User({
+		username: username.toLowerCase(),
+		password,
+		status: true,
+	});
+	try {
+		await newUser.save();
+		res.status(200).send({
+			msg: 'User Created, Welcome',
+			username: newUser.username,
+			status,
+		});
+	} catch (error) {
+		res.status(400).send({ error });
+	}
 });
-route.patch("/reset", async (req: Request, res: Response) => {
-  try {
-    await User.updateMany({}, { $set: { status: false } });
-    res.status(200).send({ msg: "all users status reset" });
-  } catch (error) {
-    res.status(400).send({ error });
-  }
+route.patch('/reset', async (req: Request, res: Response) => {
+	try {
+		await User.updateMany({}, { $set: { status: false } });
+		res.status(200).send({ msg: 'all users status reset' });
+	} catch (error) {
+		res.status(400).send({ error });
+	}
 });
 export default route;
