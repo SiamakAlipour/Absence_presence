15ba161174e614fa24fa34402040755c28d35cb7
siamak alipour <siamakalipour37@gmail.com>
2022-05-29 12:55:46 +0430

    PATCH added

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,27 +1,34 @@
-import { Method } from './../constants/types/index';
-import { API_URL, POST, GET } from './../constants/index';
-import axios from 'axios';
+import { Method } from "./../constants/types/index";
+import { API_URL, POST, GET, PATCH } from "./../constants/index";
+import axios from "axios";
 
 type Request = (url: string, method: Method, data: any) => Promise<unknown>;
 
 const request: Request = (url, method, data) => {
-	switch (method) {
-		case GET:
-			return new Promise((resolve, reject) => {
-				axios
-					.get(API_URL + url)
-					.then((res) => resolve(res.data))
-					.catch((err) => reject(err));
-			});
-		case POST:
-			return new Promise((resolve, reject) => {
-				axios
-					.post(API_URL + url, data)
-					.then((res) => resolve(res.data))
-					.catch((err) => reject(err));
-			});
-		default:
-			return Promise.reject(new Error());
-	}
+  switch (method) {
+    case GET:
+      return new Promise((resolve, reject) => {
+        axios
+          .get(API_URL + url)
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err));
+      });
+    case POST:
+      return new Promise((resolve, reject) => {
+        axios
+          .post(API_URL + url, data)
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err));
+      });
+    case PATCH:
+      new Promise((resolve, reject) => {
+        axios
+          .patch(API_URL + url)
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err));
+      });
+    default:
+      return Promise.reject(new Error());
+  }
 };
 export default request;
