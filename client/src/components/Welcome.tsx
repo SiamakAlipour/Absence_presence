37e3e88163c9491ca6c05e512b3eb7294241fccd
siamak alipour <siamakalipour37@gmail.com>
2022-05-30 16:26:25 +0430

    admin button added to Welcome component

--- client/src/components/Welcome.tsx
+++ client/src/components/Welcome.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { getCookie } from '../helpers/cookie';
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { getCookie } from "../helpers/cookie";
+import "./Welcome.css";
 
 function Welcome() {
-  const user = getCookie('login');
+  const user = getCookie("login");
   if (!user) {
     return <Navigate to="/login" />;
   }
-  return <h1 className="row align-items-center">Welcome</h1>;
+  return (
+    <div className="welcome">
+      {user === "admin" && <Link to="/admin">admin</Link>}
+      <h1 className="row align-items-center">Welcome</h1>
+    </div>
+  );
 }
 
 export default Welcome;
