9bfbf9ad155e89557dcfab82db7fdc0e3b0c2d14
siamak alipour <siamakalipour37@gmail.com>
2022-05-30 08:48:00 +0430

    DELETE added for request

--- client/src/helpers/api.ts
+++ client/src/helpers/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios';
-import { Method } from '../constants/types/index';
-import { API_URL, POST, GET, PATCH } from '../constants/index';
+import axios from "axios";
+import { Method } from "../constants/types/index";
+import { API_URL, POST, GET, PATCH, DELETE } from "../constants/index";
 
 type Request = (url: string, method: Method, data: any) => Promise<unknown>;
 
@@ -24,6 +24,14 @@ const request: Request = (url, method, data) => {
       return new Promise((resolve, reject) => {
         axios
           .patch(API_URL + url)
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err));
+      });
+    case DELETE:
+      console.log(data);
+      return new Promise((resolve, reject) => {
+        axios
+          .delete(API_URL + url, data)
           .then((res) => resolve(res.data))
           .catch((err) => reject(err));
       });
