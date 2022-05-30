4bdf349c37eba44003a833c56d0f3ff22fbc2242
siamak alipour <siamakalipour37@gmail.com>
2022-05-30 16:46:16 +0430

    some refactors done

---
+++ client/src/components/Welcome/Welcome.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import { getCookie } from "../../helpers/cookie";
+import "./Welcome.css";
+
+function Welcome() {
+  const user = getCookie("login");
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+  return (
+    <div className="welcome">
+      {user === "admin" && <Link to="/admin">admin</Link>}
+      <h1 className="row align-items-center">Welcome</h1>
+    </div>
+  );
+}
+
+export default Welcome;
